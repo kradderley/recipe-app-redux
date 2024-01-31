8e0f7dfcd2be6fe3f50921feceef982b0ed6aefd
Khes Adderley <adderleyrkhes@gmail.com>
2024-01-31 14:13:46 -0500

    random recipe added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import axios from "axios";
+import { apiRandomRecipeKey } from "../../secret";
 
 const Home = () => {
-  return <></>;
+  // const api = process.env.REACT_APP_API_KEY_ONE;
+
+  const getData = async () => {
+    const { data } = await axios.get(
+      `https://api.spoonacular.com/recipes/random?number=12&apiKey=${apiRandomRecipeKey}`
+    );
+    console.log(data.recipes);
+  };
+
+  getData();
+
+  return <> </>;
 };
 
 export default Home;
